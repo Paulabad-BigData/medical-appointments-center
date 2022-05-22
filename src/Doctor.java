33cde1bd7ec1d5cfc55f7c677440cb9688dfd2c1
Paula Abad <paulabad@gmail.com>
2022-05-22 12:10:17 -0500

    feat(backend): Doctor class creation

---
+++ src/Doctor.java
@@ -0,0 +1,14 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    /**
+     * behaviors or methods:
+     * 1- The first method is showName to show the name of the doctor.
+     */
+    public void showName( ){
+        System.out.println(name);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Paula Abad";
+        myDoctor.showName();
+    }
+}
