947c3e423bc1e62eb1b98538be2dea6e17d4f7ee
Paula Abad <paulabad@gmail.com>
2022-05-22 15:11:24 -0500

    feat(backend): Project Interface

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,14 +1,28 @@
 public class Doctor {
-    int id;
+    static int id = 1; // Autoincrement
     String name;
     String speciality;
+
+    Doctor() {
+        System.out.println("Construyendo el objeto Doctor");
+        id++;
+    }
+
+    Doctor(String name) {
+        System.out.println("El nombre del Doctor es: " + name);
+    }
 
     /**
      * behaviors or methods:
      * 1- The first method is showName to show the name of the doctor.
+     * 2- Method id autoincrement show
      */
     public void showName( ){
         System.out.println(name);
+    }
+
+    public void showId() {
+        System.out.println("ID Doctor: " + id);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,73 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Andrea Tejada");
         myDoctor.name = "Paula Abad";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor.id++;
+
+        Doctor MyDoctorAnn = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+    }
+
+    public static void showMenu(){
+        System.out.println("Welcome to My Appointments");
+        System.out.println("Selecciona la opción deseada");
+
+        int response = 0;
+        do {
+            System.out.println("1. Doctor");
+            System.out.println("2. Patient");
+            System.out.println("0. Salir");
+
+            Scanner sc = new Scanner(System.in);
+            response = Integer.valueOf(sc.nextLine());
+
+            switch (response){
+                case 1:
+                    System.out.println("Doctor");
+                    break;
+                case 2:
+                    response = 0;
+                    showPatientMenu();
+
+                    break;
+                case 0:
+                    System.out.println("Thank you for you visit");
+                    break;
+                default:
+                    System.out.println("Please select a correct answer");
+            }
+        }while (response != 0);
+    }
+
+    public static void showPatientMenu(){
+        int response = 0;
+        do {
+            System.out.println("\n\n");
+            System.out.println("Patient");
+            System.out.println("1. Book an appointment");
+            System.out.println("2. My appointments");
+            System.out.println("0. Return");
+
+            Scanner sc = new Scanner(System.in);
+            response = Integer.valueOf(sc.nextLine());
+
+            switch (response){
+                case 1:
+                    System.out.println("::Book an appointment");
+                    break;
+                case 2:
+                    System.out.println("::My appointments");
+                    break;
+                case 0:
+                    showMenu();
+                    break;
+            }
+        }while (response != 0);
     }
 }
